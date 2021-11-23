afae963c76e24103efdab4edef79f602510b75ab
Cristiellen CArvalho <cristiellen57@gmail.com>
2021-11-23 18:22:19 -0300

    First commit

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -9,8 +9,9 @@ public class primeiroPrograma {
         System.out.println(gato);
 
        /* int a = 5 ;
-        int b = 3 ;
+        int b = 3 ;  */
 
-        System.out.println("Hello World!" + (a+b));*/
+
+
     }
 }
